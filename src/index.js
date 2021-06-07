c197be7f83d8400412e83ca3aeeed98bdedd771a
andriy-lanyk <LAM1988@ukr.net>
2021-06-07 20:42:07 +0300

    change import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import debounce from 'lodash.debounce';
 import modal from 'picoModal';
-import { alert, notice, info, success, error } from '@pnotify/core';
+import { success, error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import './sass/main.scss';
